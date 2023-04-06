61dd1734552c894999c16803501f00125537e5de
Dide Aydogan <dide1992@gmail.com>
2023-04-05 22:46:02 -0400

    logo created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from "./components/Header"
-
+import Footer from "./components/Footer"
 
 function App() {
   const URL = "http://localhost:4000/"
   return (
     <div className='Nav'>
       <Header />
+      <Footer />
     </div>
 
     )
