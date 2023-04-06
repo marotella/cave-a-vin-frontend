f56aa4edb9463e6d0c212a7685b5bc3354bb01da
Dide Aydogan <dide1992@gmail.com>
2023-04-05 21:15:11 -0400

    header.js nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,33 @@
-//fghfvkhgf
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Header() {
+    return (
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" style={{color:"white"}} href="#">CAVE A VIN</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" style={{color:"white"}} aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" style={{color:"white"}} href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" style={{color:"white"}} >Wine Selections</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" className='btn1' type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    );
+}
+export default Header
