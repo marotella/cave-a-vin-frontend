949220129f0cc1c3f6504004986f6154d3583d21
Dide Aydogan <dide1992@gmail.com>
2023-04-07 11:29:27 -0400

    created pages and Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+        <div>
+            <h2>From Our Cellar to Your Glass..</h2>
+            <p className="quote">Drinking good wine with good food in good company is one of life’s most civilized pleasures.</p>
+            <p className="author">-Michael Broadbent</p>
+            <button className="cellar"> Explore Our Wine Cellar</button>
+        </div>
+    )
+  }
+  
+  export default Home;
+  
