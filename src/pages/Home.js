7df35964331cdc86452531f256fd4b464fccde6f
Dide Aydogan <dide1992@gmail.com>
2023-04-08 00:56:43 -0400

    homepage css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
 function Home() {
     return (
-        <div>
-            <h2>From Our Cellar to Your Glass..</h2>
-            <p className="quote">Drinking good wine with good food in good company is one of life’s most civilized pleasures.</p>
-            <p className="author">-Michael Broadbent</p>
-            <button className="cellar"> Explore Our Wine Cellar</button>
+      
+        <div className="container1" style={{ width: "60%" }}>
+        <div> 
+        <h2>From Our Cellar to Your Glass..</h2>
         </div>
+            <p className="quote">"Drinking good wine with good food in good company is one of life’s most civilized pleasures."</p>
+            <p className="author">-Michael Broadbent</p>
+            <button className="explore"> Explore Our Wine Cellar</button>
+           {/* <iframe src='https://gfycat.com/ifr/RichHilariousCony'></iframe><p><a href="https://gfycat.com/richhilariouscony">via Gfycat</a></p> */}
+        <div className="container2" style={{ width: "100%" }}></div>
+        </div>
+     
     )
   }
-  
   export default Home;
   
