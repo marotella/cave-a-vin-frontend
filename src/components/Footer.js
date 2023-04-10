b431db92234a352f3008d5d95fabc3e22533b38b
Dide Aydogan <dide1992@gmail.com>
2023-04-10 15:22:57 -0400

    homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,8 @@
 function Footer(props) {
     return (
       <footer className="footer">
-        <div className="socialmedia">
-        <a href="mailto:caveavin@example.com">
-          <img className='emailimg' src="https://i.imgur.com/repqUlu.png" alt="Facebook" />
-          </a>
-          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-            <img className='fbimg' src="https://i.imgur.com/9D6FJkh.png" alt="Facebook" />
-          </a>
-          <a
-            href="https://www.instagram.com/explore/tags/wine/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img className='igimg' src="https://i.imgur.com/i96mrRb.png" alt="Instagram" />
-          </a>
-          <p>&copy; {new Date().getFullYear()} Cave a Vin. All rights reserved.</p>
+       <div>
+          <p>&copy; {new Date().getFullYear()} Cave A Vin. All rights reserved.</p>
         </div>
   
       </footer>
