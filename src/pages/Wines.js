72d10a6a3f5218484819b1ba39d4b4419b23707d
Dide Aydogan <dide1992@gmail.com>
2023-04-11 20:29:27 -0400

    pouring.. style

--- src/pages/Wines.js
+++ src/pages/Wines.js
@@ -12,7 +12,7 @@ const Wines = (props) => {
   };
 
   const loading = () => {
-    return <h1>Pouring...</h1>;
+    return <h1 style={{color:"#943330", fontFamily:'Courier New', letterSpacing:'-0.6px', position:"relative", left:"70px"}}>Pouring...</h1>;
   };
   return (props.wineData ? loaded() : loading());
 }
