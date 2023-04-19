43f7ddf6875b75414db786696589ab38e1acbe27
marotella <michelle.a.rotella@gmail.com>
2023-04-18 22:21:52 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -52,24 +52,13 @@ function App() {
       console.error(error);
     }}
 
-    //create
-    const createWine = async (_id, createdWine) => {
-      try{
-        const response = await fetch (`${URL}wines/${_id}`, {
+    const createWine = async (newWine) => {
+      await fetch (URL + newWine, {
           method:"POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(createdWine),
-        });
-        if (!response.ok) {
-          throw new Error("Failed to create wine");
-        }
-        getWineData();
-      } catch (error) {
-        console.error(error)
-      }
-    };
+        })
+        getWineData()
+    }
+    console.log(wineData)
 
     //update
     const updateWine = async (_id, updatedWine) => {
@@ -106,7 +95,7 @@ return (
       <Route exact path="/" element={<Home URL={URL} />} />
       <Route exact path="/about" element={<About URL={URL} />} />
       <Route exact path="/register" element={<Register URL={URL} />} />
-      <Route exact path="/wines/:_id/new" element={<New wineData={wineData} createWine={createWine}/>} />
+      <Route exact path="/new" element={<New URL={URL} createWine={createWine}/>} />
       <Route exact path="/wines" element={<Wines wineData={wineData} URL={URL} />} />
       <Route exact path="/wines/:_id" element={<Wine wineData={wineData} URL={URL} deleteWine={deleteWine} getWineData={getWineData} />} />
       <Route exact path="/wines/:_id/edit" element={<Edit wineData={wineData} updateWine={updateWine} />} />
